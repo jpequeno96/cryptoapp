59fc837097dc998e97212adcd3c3d1bfbab4f30b
jpequeno96 <joaopequeno1886@gmail.com>
2022-12-06 16:04:20 +0000

    feat: globalStats API call

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+import store from './app/store';
+
 
 ReactDOM.render(
-<Router>
-<App/>
-</Router>, document.getElementById('root'));
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>,
+  document.getElementById('root'),
+);
